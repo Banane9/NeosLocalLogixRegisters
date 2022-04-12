ce277a3c3bcc8c145201c808343762810a0f7eda
Arne Kiesewetter <banane9@live.de>
2022-04-12 21:22:40 +0200

    Make things work for UserRegisters, cleanup code.

--- LocalLogixRegisters/LocalLogixRegisters.cs
+++ LocalLogixRegisters/LocalLogixRegisters.cs
@@ -29,13 +29,12 @@ namespace LocalLogixRegisters
         {
             Harmony harmony = new Harmony($"{Author}.{Name}");
             Config = GetConfiguration();
-            Config.OnThisConfigurationChanged += e => e.Config.Save(true);
             Config.Save(true);
             harmony.PatchAll();
         }
 
         [HarmonyPatch(typeof(LogixTip))]
-        private static class LogixTipPatch
+        private static class LogixTipPatches
         {
             private const string localizedPrefix = "Localized ";
 
@@ -43,6 +42,7 @@ namespace LocalLogixRegisters
             private static readonly Type[] copyTypes = new[] { typeof(ValueCopy<>), typeof(ReferenceCopy<>) };
 
             private static readonly Dictionary<LogixTip, Slot> lastCreatedSlot = new Dictionary<LogixTip, Slot>();
+
             private static readonly string[] targetFieldNames = new[] { "Value", "Target", "Target", "User" };
             private static readonly Type[] targetTypes = new[] { typeof(ValueRegister<>), typeof(ReferenceRegister<>), typeof(SlotRegister), typeof(UserRegister) };
 
@@ -57,63 +57,42 @@ namespace LocalLogixRegisters
             [HarmonyPatch(nameof(LogixTip.GenerateMenuItems))]
             private static void GenerateMenuItemsPostfix(LogixTip __instance, ContextMenu menu)
             {
+                if (!boolFields.ContainsKey(__instance) || boolFields[__instance] == null)
+                {
+                    var local = __instance.Slot.AttachComponent<ValueField<bool>>();
+
+                    local.Persistent = false;
+                    local.Value.Value = LocalLogixRegisters.Config.GetValue(LocalLogixRegisters.LocalDefault);
+                    local.MarkChangeDirty();
+
+                    boolFields.Add(__instance, local);
+                }
+
                 menu.AddToggleItem(boolFields[__instance].Value,
-                    "Creating Localized Registers", "Creating Synchronized Registers", color.Green, color.Red,
+                    "Creating Localized Registers", "Creating Synchronized Registers", color.Pink, color.White,
                     new Uri("neosdb:///12db534404a43b1662c90771882d624ed8505a39c9cb6ed898009d456c88d8fe"),
                     new Uri("neosdb:///eed4447d3cc06e57230a94821bde65d310f4a561017fefc4970f4f80d0a66ddc"));
-            }
-
-            [HarmonyPostfix]
-            [HarmonyPatch("OnDestroy")]
-            private static void OnDequippedPostfix(LogixTip __instance)
-            {
-                if (!boolFields.ContainsKey(__instance))
-                    return;
-
-                boolFields[__instance]?.Destroy();
-                boolFields.Remove(__instance);
-            }
-
-            [HarmonyPostfix]
-            [HarmonyPatch(nameof(LogixTip.OnEquipped))]
-            private static void OnEquippedPostfix(LogixTip __instance)
-            {
-                if (boolFields.ContainsKey(__instance))
-                    return;
-
-                var local = __instance.Slot.AttachComponent<ValueField<bool>>();
-                local.Value.Value = Config.GetValue(LocalDefault);
-                boolFields.Add(__instance, local);
             }
 
             [HarmonyPostfix]
             [HarmonyPatch("SpawnNode")]
             private static void SpawnNodePostfix(LogixTip __instance, SyncType ___ActiveNodeType)
             {
-                if (!boolFields.TryGetValue(__instance, out var local) || !local.Value.Value)
+                if (!boolFields.TryGetValue(__instance, out var local) || !local.Value)
                     return;
 
                 var type = ___ActiveNodeType.Value;
-
-                if (type == null || (!targetTypes.Contains(type) && !type.IsGenericType && !targetTypes.Contains(type.GetGenericTypeDefinition())) || !lastCreatedSlot.TryGetValue(__instance, out var slot))
+                if (type == null || (!targetTypes.Contains(type) && !type.IsGenericType && !targetTypes.Contains(type.GetGenericTypeDefinition()))
+                 || !lastCreatedSlot.TryGetValue(__instance, out var slot))
                     return;
 
                 var register = slot.GetComponent(type);
-
                 if (register == null)
                     return;
 
                 var typeIndex = type.IsGenericType ? (type.GetGenericTypeDefinition() == targetTypes[0] ? 0 : 1) : (type == targetTypes[2] ? 2 : 3);
-                Msg("Target Register: " + register.ToString() + " with Members: " + string.Join("; ", register.SyncMembers.Select(m => m.Name + " is initing: " + m.IsInInitPhase)));
-                var field = register.TryGetField(targetFieldNames[typeIndex]);
 
-                Msg("User: " + ((UserRegister)register).User + " is null: " + ((UserRegister)register).User == null);
-                Msg("Linking to " + field + " with Type " + type.ToString());
-                if (typeIndex == 3)
-                {
-                    field = (IField)Traverse.Create(register).Field("User").Field("User").GetValue();
-                    Msg("Linking to " + field.ToString() + " with Type " + type.ToString());
-                }
+                var field = typeIndex == 3 ? (IField)Traverse.Create(register).Field("User").Field("User").GetValue() : register.TryGetField(targetFieldNames[typeIndex]);
 
                 if (typeIndex == 0)
                     ValueCopyExtensions.DriveFrom(field, field, true, true, false);
